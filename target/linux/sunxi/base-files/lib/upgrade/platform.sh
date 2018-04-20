93c45776ceb12ea26b6cf6e9191a1ad45eb14597
chengping <13641793410@163.com>
2018-04-21 00:59:18 +0800

    fix firmware upgrade

--- target/linux/sunxi/base-files/lib/upgrade/platform.sh
+++ target/linux/sunxi/base-files/lib/upgrade/platform.sh
@@ -1,7 +1,12 @@
 platform_check_image() {
-	return 0;
+	return 0
 }
 
-platform_do_upgrade() {
-	get_image "$@" | dd of="/dev/mtd4" bs=4096 conv=fsync
+platform_do_upgrade() {	
+	mtd write /tmp/firmware.img rootfs	
+	if [ "$SAVE_CONFIG" -eq 1 ]; then
+		ehco "keep config"
+	else
+		mtd erase rootfs_data
+	fi
 }
