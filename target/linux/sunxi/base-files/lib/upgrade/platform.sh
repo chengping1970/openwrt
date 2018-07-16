03b1f6fed30a87198f1f2eb84e297f4ccff8428c
chengping <13641793410@163.com>
2018-07-16 11:12:07 +0800

    add reset key and LED

--- target/linux/sunxi/base-files/lib/upgrade/platform.sh
+++ target/linux/sunxi/base-files/lib/upgrade/platform.sh
@@ -3,6 +3,7 @@ platform_check_image() {
 }
 
 platform_do_upgrade() {	
+	echo "1" > /sys/class/leds/red\:status/brightness
 	mtd write /tmp/firmware.img rootfs	
 	if [ "$SAVE_CONFIG" -eq 1 ]; then
 		ehco "keep config"
